aa3c9d904e0ea876c6eafb9f85b2b050bfd2ffe0
seungminjin <dryjins@gmail.com>
2019-01-11 08:20:15 +0300

    write generator and test
      GET /services/generator/next
    GET /services/generator/next 500 5.725 ms - 41
        √ should error out error out if the res is not 500 and res.body is not {"message":"The generator is not ready."}
    
      POST /services/generator
    POST /services/generator 200 12.862 ms - 82
        √ should error out error out if the res is not 200 and res.body is not {"message": "partialSumSeq is generated with args (1,3,7,2,0), pipelines (undefined)"}
    
      GET /services/generator/next
    GET /services/generator/next 200 0.877 ms - 39
        √ should error out error out if the res is not 200 and res.body is not { "value":{"status":false,"number":"1"}}
    GET /services/generator/next 200 0.299 ms - 38
        √ should error out error out if the res is not 200 and res.body is not { "value":{"status":true,"number":"4"}}
    GET /services/generator/next 200 0.305 ms - 40
        √ should error out error out if the res is not 200 and res.body is not { "value":{"status":false,"number":"11"}}
    GET /services/generator/next 200 0.311 ms - 40
        √ should error out error out if the res is not 200 and res.body is not { "value":{"status":false,"number":"13"}}
    GET /services/generator/next 200 0.271 ms - 40
        √ should error out error out if the res is not 200 and res.body is not { "value":{"status":false,"number":"13"}}
    GET /services/generator/next 500 0.277 ms - 45
        √ should error out error out if the res is not 200 and res.body is not  "message": "The generator is out of values."

---
+++ backend/routes/services/generator.js
@@ -0,0 +1,54 @@
+const services = require('express').Router();
+const {pipeSeq} = require('./pipeSeq/pipeSeq');
+const sequencers = require('./sequencers');
+const pipelines = require('./pipeSeq/pipelines');
+var currentGen;
+
+function generator(sequencer, pipelines, ...args) {
+  let seq = pipeSeq(sequencer, ...args);
+  if (pipelines)
+    pipelines.forEach(pipe => {
+      seq.pipeline(pipe);
+    });
+  seq = seq.invoke();
+  return {
+    next: function () {
+      let o = seq();
+      return o;
+    }
+  };
+}
+
+services.post('/generator', function (req, res) {
+  if (!req.body.sequencer) {
+    res.status(500);
+    res.send({message: 'The sequencer parameter is required.'});
+  } else {
+    currentGen = generator(sequencers[req.body.sequencer], req.body.pipelines.map(p => {
+      return pipelines[p]
+    }), ...req.body.args);
+    res.status(200).json({
+      message: req.body.sequencer
+        + ' is generated with args (' + req.body.args
+        + '), pipelines ('
+        + req.body.pipelines + ')'
+    })
+  }
+});
+
+services.get('/generator/next', function (req, res) {
+  if (!currentGen) {
+    res.status(500);
+    res.send({message: 'The generator is not ready.'});
+  } else {
+    let value = currentGen.next();
+    if (value === undefined) {
+      res.status(500);
+      res.send({message: 'The generator is out of values.'});
+    } else {
+      res.status(200).json({value});
+    }
+
+  }
+});
+module.exports = services;
