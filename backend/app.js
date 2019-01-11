e11677bb597b7f672420aca59834972252c214e0
seungminjin <dryjins@gmail.com>
2019-01-11 06:32:00 +0300

    change app settings.

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+BigInt.prototype.toJSON = function() {return this.toString(); }; // for big integer, this app uses string.
 
-var app = express();
+const app = express();
+const routes = require('./routes')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +15,12 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// add routes
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
