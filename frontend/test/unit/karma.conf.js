37149ac944ed5620cc4a29bfc74c79c4de2a7bdd
seungminjin <dryjins@gmail.com>
2019-01-12 10:47:34 +0300

    add build

---
+++ frontend/test/unit/karma.conf.js
@@ -0,0 +1,46 @@
+// This is a karma config file. For more details see
+//   http://karma-runner.github.io/0.13/config/configuration-file.html
+// we are also using it with karma-webpack
+//   https://github.com/webpack/karma-webpack
+
+const webpackConfig = require('../../build/webpack.test.conf');
+
+module.exports = function karmaConfig(config) {
+  config.set({
+    // to run in additional browsers:
+    // 1. install corresponding karma launcher
+    //    http://karma-runner.github.io/0.13/config/browsers.html
+    // 2. add it to the `browsers` array below.
+    browsers: [
+      'ChromeHeadless'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    reporters: ['spec', 'coverage'],
+    files: [ // '../../node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
+      // '../unit/**/*.spec.js',
+      './index.js'],
+    preprocessors: {
+      './index.js': ['webpack', 'sourcemap'],
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true,
+    },
+    proxies: {
+      '/services': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/services': '',
+        },
+      },
+    },
+    coverageReporter: {
+      dir: './coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' },
+      ],
+    },
+  });
+};
